4a344e41415056597abc19b74df8a175d9441700
keenanhumm <keenan.humm@huskers.unl.edu>
2019-09-09 06:44:03 -0500

    producer consumer implementation

--- src/com/kaytec/utils/ThreadColor.java
+++ src/com/kaytec/utils/ThreadColor.java
@@ -8,4 +8,6 @@ public class ThreadColor {
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_YELLOW = "\u001b[33m";
+    public static final String ANSI_WHITE = "\u001b[37m";
 }
